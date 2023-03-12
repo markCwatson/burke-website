b3d5b8c8bfef53db95bafa9b296870f1d85244fb
Mark Watson <mwatson@prosaris.ca>
2023-03-12 20:14:28 -0300

    mdb-footer

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## Website for BJB Renos
 
-Available for preview [here](https://arcane-beach-38345.herokuapp.com/).
+Available for preview at [www.bjbrenos.ca](https://bjbrenos.ca).
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -9,18 +9,18 @@ function Navbar() {
   const navRef = useRef();
 
   const showNavBar = () => {
-    navRef.current.classList.toggle("navbar-responsive");
+    navRef.current.classList.toggle("bjb-navbar-responsive");
   };
 
   return (
     <>
       <header>
-        <div className="navbar-logo-container">
+        <div className="bjb-navbar-logo-container">
           <a href="/#Home">
-            <img src="/logo.png" alt="" className="navbar-logo" />
+            <img src="/logo.png" alt="" className="bjb-navbar-logo" />
           </a>
         </div>
-        <nav ref={navRef} className="navbar">
+        <nav ref={navRef} className="bjb-navbar">
           <a href="/#services" onClick={showNavBar}>
             Services
           </a>
@@ -32,13 +32,13 @@ function Navbar() {
           </a>
           <Quote />
           <button
-            className="nabar-mobile-btn navbar-btn-close"
+            className="bjb-navbar-mobile-btn bjb-navbar-btn-close"
             onClick={showNavBar}
           >
             <FaTimes />
           </button>
         </nav>
-        <button className="nabar-mobile-btn" onClick={showNavBar}>
+        <button className="bjb-navbar-mobile-btn" onClick={showNavBar}>
           <FaBars size={16} />
         </button>
       </header>
